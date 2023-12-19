aef5d5226721aa509907e5cc78dedbbc2236e1a9
amali <alugar1951@ieselcaminas.oreg>
2023-12-19 19:33:54 +0100

    fin definitivo

--- app/src/main/java/com/example/myreminders_garciadopcio/Nueva.kt
+++ app/src/main/java/com/example/myreminders_garciadopcio/Nueva.kt
@@ -1,11 +1,18 @@
 package com.example.myreminders_garciadopcio
 
 import android.annotation.SuppressLint
+import android.content.res.Configuration
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Arrangement.Center
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuDefaults.outlinedTextFieldColors
@@ -27,6 +34,7 @@ import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import kotlinx.coroutines.launch
@@ -37,6 +45,25 @@ import kotlinx.coroutines.launch
 fun new(navHostController: NavHostController, viewModel: NotesViewModel){
 /**In this function we put together everything we want to show on the screen*/
 
+    val configuration = LocalConfiguration.current
+
+    when (configuration.orientation) {
+        Configuration.ORIENTATION_LANDSCAPE -> {
+                newHorizontal(navHostController, viewModel)
+
+
+        }
+        else -> {
+            newVertical(navHostController, viewModel)
+        }
+    }
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun newVertical(navHostController: NavHostController, viewModel: NotesViewModel){
     val snackbarHostState = remember { SnackbarHostState() }
     val scope = rememberCoroutineScope()
 
@@ -118,5 +145,106 @@ fun new(navHostController: NavHostController, viewModel: NotesViewModel){
     }
 }
 
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun newHorizontal(navHostController: NavHostController, viewModel: NotesViewModel){
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+
+    Scaffold (
+
+        topBar = {
+            TopAppBarNew(navHostController = navHostController)
+        }
+    ) {
+
+        var title by remember { mutableStateOf("") }
+        var description by remember { mutableStateOf("") }
+
+        /**In this column we create two OutlinedTextField so that the client can enter a title and a description for their new note*/
+        Row {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = it.calculateTopPadding())
+                    .weight(1f)
+            ) {
+
+
+
+                OutlinedTextField(
+                    value = title,
+                    onValueChange = { title = it },
+                    label = {
+                        Text("Title")
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp),
+                    singleLine = true,
+                    colors = outlinedTextFieldColors(
+                        focusedBorderColor = Color.Transparent,
+                        unfocusedBorderColor = Color.Transparent,
+                        //textColor = LocalContentColor.current
+                    ),
+                )
+
+                OutlinedTextField(
+                    value = description,
+                    onValueChange = { description = it },
+                    label = {
+                        Text("Description",)
+                    },
+
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp),
+//                    .height(200.dp)
+                    singleLine = false,
+                    colors = outlinedTextFieldColors(
+                        focusedBorderColor = Color.Transparent,
+                        unfocusedBorderColor = Color.Transparent,
+                        //textColor = LocalContentColor.current
+                    ),
+                )
+            }
+            Column (
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = it.calculateTopPadding())
+                    .weight(1f),
+                horizontalAlignment = CenterHorizontally,
+                verticalArrangement = Center
+            ){
+                /**This SnackbarHost shows a message once the filled information has been sent*/
+                SnackbarHost(hostState = snackbarHostState, modifier = Modifier.align(CenterHorizontally))
+
+                /**This button sends the information filled out by the client to the database.*/
+                Button(
+                    onClick = {
+                        val note = Note(title, description)
+                        viewModel.addNote(note)
+
+                        scope.launch { snackbarHostState.showSnackbar("The note has been added successfully")}
+
+                        description = ""
+                        title = ""
+                    },
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .align(CenterHorizontally)
+                ) {
+                    Text(text = "Add note", color = Color.Black)
+                }
+            }
+
+
+        }
+
+
+        }
+    }
 
 
